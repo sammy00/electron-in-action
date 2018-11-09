618dfdbb584e4d7108c300ec6e927e3393f672bf
sammy00 <xiangminli@outlook.com>
2018-11-09 16:52:55 +0800

    Adding script with Node.js global in the browser context

--- chapter02/bookmarker/app/main.js
+++ chapter02/bookmarker/app/main.js
@@ -8,4 +8,8 @@ let mainWindow = null;
 app.on('ready', () => {
   console.log('Hello from Electron');
   mainWindow = new(BrowserWindow);
+  //  Loading an HTML document into the main window: ./app/main.js
+  mainWindow.loadFile(__dirname+'/index.html');
+  // open dev tool 
+  //mainWindow.webContents.openDevTools();
 });
