ba81eae6f6aeffd45bd3624cee1f204ac6659736
sammy00 <xiangminli@outlook.com>
2018-11-10 17:51:12 +0800

    done file dragger and dropper

--- chapter06/firesale/app/renderer.js
+++ chapter06/firesale/app/renderer.js
@@ -20,6 +20,12 @@ const openInDefaultButton = document.querySelector('#open-in-default');
 let filePath = null;
 let originalContent = '';
 
+// Setting up foundation for drag-and-drop events
+document.addEventListener('dragstart', (event) => event.preventDefault());
+document.addEventListener('dragover', (event) => event.preventDefault());
+document.addEventListener('dragleave', (event) => event.preventDefault());
+document.addEventListener('drop', (event) => event.preventDefault());
+
 ipcRenderer.on('file-opened', (event, file, content) => {
   // Updates the path of the currently opened file stored in the top-level scope
   filePath = file;
@@ -32,6 +38,29 @@ ipcRenderer.on('file-opened', (event, file, content) => {
   // Calls the method that updates the window’s title bar whenever
   // a new file is opened.
   updateUserInterface();
+});
+
+markdownView.addEventListener('dragover', (event) => {
+  const file = getDraggedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    markdownView.classList.add('drag-over');
+  } else {
+    markdownView.classList.add('drag-error');
+  }
+});
+
+markdownView.addEventListener('drop', (event) => {
+  const file= getDroppedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    main.openFile(currentWindow,file.path);
+  }else {
+    alert('That file type is not supported');
+  }
+
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
 });
 
 markdownView.addEventListener('keyup', (event) => {
@@ -63,6 +92,13 @@ saveHtmlButton.addEventListener('click', () => {
 saveMarkdownButton.addEventListener('click', () => {
   main.saveMarkdown(currentWindow, filePath, markdownView.value);
 });
+
+const fileTypeIsSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
 
 const renderMarkdownToHTML = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
