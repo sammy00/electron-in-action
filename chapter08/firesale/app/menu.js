888ff7861b0d65837bcd4e6ed37c65bd0e01e3b0
sammy00 <xiangminli@outlook.com>
2018-11-12 19:57:56 +0800

    add in support for open in default app and show file

---
+++ chapter08/firesale/app/menu.js
@@ -0,0 +1,197 @@
+const { app, dialog, BrowserWindow, Menu, shell } = require('electron');
+const main = require('./main');
+
+// template serves as the blueprint for the menu
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'New File',
+        accelerator: 'CommandOrControl+N',
+        click() {
+          main.createWindow();
+        },
+      },
+      {
+        label: 'Open File',
+        accelerator: 'CommandOrControl+O',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            return main.getFileFromUser(focusedWindow);
+          }
+
+          const newWindow = main.createWindow();
+          newWindow.on('show', () => {
+            main.getFileFromUser(focusedWindow);
+          });
+        },
+      },
+      {
+        label: 'Save File',
+        accelerator: 'CommandOrControl+S',
+        click(item, focusedWindow) {
+          if (!focusedWindow) {
+            return dialog.showErrorBox(
+              'Cannot Save or Export',
+              'There is currently no active document to save or export.'
+            );
+          }
+          focusedWindow.webContents.send('save-markdown');
+        },
+      },
+      {
+        label: 'Export HTML',
+        accelerator: 'Shift+CommandOrControl+S',
+        click(item, focusedWindow) {
+          if (!focusedWindow) {
+            return dialog.showErrorBox(
+              'Cannot Save or Export',
+              'There is currently no active document to save or export.'
+            );
+          }
+          focusedWindow.webContents.send('save-html');
+        },
+      },
+    ],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CommandOrControl+Z',
+        role: 'undo',
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CommandOrControl+Z',
+        role: 'redo',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Cut',
+        accelerator: 'CommandOrControl+X',
+        role: 'cut',
+      },
+      {
+        label: 'Copy',
+        accelerator: 'CommandOrControl+C', // keyboard shortcut
+        role: 'copy',
+      },
+      {
+        label: 'Paste',
+        accelerator: 'CommandOrControl+V',
+        role: 'paste',
+      },
+      {
+        label: 'Select All',
+        accelerator: 'CommandOrControl+A',
+        role: 'selectall',
+      },
+    ],
+  },
+  {
+    label: 'Window',
+    // The window role on the Window menu causes Electron to add a list of
+    // all open windows at the end of the menu when running in macOS
+    role: 'window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CommandOrControl+M',
+        role: 'minimize',
+      },
+      {
+        label: 'Close',
+        accelerator: 'CommandOrControl+W',
+        role: 'close',
+      },
+    ],
+  },
+  {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Visit Website',
+        click() {},
+      },
+      {
+        label: 'Toggle Developer Tool',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            focusedWindow.webContents.toggleDevTools();
+          }
+        },
+      },
+    ],
+  },
+];
+
+// Gets the name of the application. This won’t show up in the menu now
+// but is useful down the road. This targets the MacOS.
+if ('darwin' === process.platform) {
+  //template.unshift({ label: 'Fire Sale' });
+  const appName = app.getName();
+  template.unshift({
+    label: appName,
+    submenu: [
+      {
+        label: `About ${appName}`,
+        role: 'about',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: [],
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: `Hide ${appName}`,
+        accelerator: 'Command+H',
+        role: 'hide',
+      },
+      {
+        label: 'Hide Others',
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers',
+      },
+      {
+        label: 'Show All',
+        role: 'unhide',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: `Quit ${appName}`,
+        accelerator: 'Command+Q',
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  });
+
+  const windowMenu = template.find((item) => 'Window' === item.label);
+  (windowMenu.role = 'window'),
+    windowMenu.submenu.push(
+      {
+        type: 'separator',
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front',
+      }
+    );
+}
+
+module.exports = Menu.buildFromTemplate(template);
---
+++ chapter08/firesale/app/renderer.js
@@ -0,0 +1,229 @@
+const { remote, shell, ipcRenderer } = require('electron');
+// Requires the Menu module from the context of the main process
+// via the remote module
+const { Menu } = remote;
+
+const path = require('path');
+
+const main = remote.require('./main.js');
+const currentWindow = remote.getCurrentWindow();
+
+const marked = require('marked');
+
+const markdownView = document.querySelector('#markdown');
+const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
+
+const fileTypeIsSupported = (file) => {
+  return ['text/plain', 'text/markdown'].includes(file.type);
+};
+
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+
+const markdownContextMenu = Menu.buildFromTemplate([
+  {
+    label: 'Open File',
+    click() {
+      main.getFileFromUser();
+    },
+  },
+  { type: 'separator' },
+  { label: 'Cut', role: 'cut' },
+  { label: 'Copy', role: 'copy' },
+  { label: 'Paste', role: 'paste' },
+  { label: 'Select All', role: 'selectall' },
+]);
+
+const openInDefaultApp = () => {
+  if (!filePath) {
+    // It seems unlikely that a user could click a disabled button, but we
+    // guard against them seeing a cryptic error by showing a more helpful
+    // one and returning from the function.
+    return alert('This file has not been saved to the filesystem.');
+  }
+
+  // Requests that the provided operating system be opened by the default
+  // application designated by the user.
+  shell.openItem(filePath);
+};
+
+const renderFile = (file, content) => {
+  // Updates the path of the currently opened file stored in the top-level scope
+  filePath = file;
+  // Updates the original content to determine if the file has unsaved changes
+  originalContent = content;
+
+  markdownView.value = content;
+  renderMarkdownToHTML(content);
+
+  // When a file path has been sent to the renderer process to be displayed,
+  // we activate the Show File and Open in Default buttons.
+  showFileButton.disabled = false;
+  openInDefaultButton.disabled = false;
+
+  // Calls the method that updates the window’s title bar whenever
+  // a new file is opened.
+  updateUserInterface(false);
+};
+
+const renderMarkdownToHTML = (markdown) => {
+  htmlView.innerHTML = marked(markdown, {
+    sanitize: true,
+  });
+};
+
+const showFile = () => {
+  if (!filePath) {
+    // It seems unlikely that a user could click a disabled button, but we
+    // guard against them seeing a cryptic error by showing a more helpful
+    // one and returning from the function.
+    return alert('This file has not been saved to the filesystem.');
+  }
+
+  // Triggers the operating system's native file browser to open a new window
+  // with the provided file path highlighted.
+  shell.showItemInFolder(filePath);
+};
+
+const updateUserInterface = (edited) => {
+  // edited indicates whether the document has unsaved changes
+  let title = 'Fire Sale';
+
+  if (filePath) {
+    // Updating the window title based on the current file
+    title = `${path.basename(filePath)} - ${title}`;
+  }
+  if (edited) {
+    title = `${title} (Edited)`;
+  }
+
+  currentWindow.setTitle(title);
+  // If edited, then updates the window accordingly
+  currentWindow.setDocumentEdited(edited);
+
+  // Enabling the Save and Revert buttons when there are unsaved changes
+  saveMarkdownButton.disabled = !edited;
+  revertButton.disabled = !edited;
+};
+
+let filePath = null;
+let originalContent = '';
+
+// Setting up foundation for drag-and-drop events
+document.addEventListener('dragstart', (event) => event.preventDefault());
+document.addEventListener('dragover', (event) => event.preventDefault());
+document.addEventListener('dragleave', (event) => event.preventDefault());
+document.addEventListener('drop', (event) => event.preventDefault());
+
+ipcRenderer.on('file-changed', (event, file, content) => {
+  remote.dialog.showMessageBox(currentWindow, {
+    type: 'warning',
+    title: 'Overwrite Current Unsaved Changes?',
+    message: 'Another application has changed this file. Load changes?',
+    buttons: ['Yes', 'Cancel'],
+    defaultId: 0,
+    cancelId: 1,
+  });
+
+  renderFile(file, content);
+});
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  if (!currentWindow.isDocumentEdited()) {
+    renderFile(file, content);
+    return;
+  }
+
+  const choice = remote.dialog.showMessageBox(currentWindow, {
+    type: 'warning',
+    title: 'Overwrite Current Unsaved Changes?',
+    message:
+      'Opening a new file in this window will overwrite your unsaved changes. Open this file anyway?',
+    buttons: ['Yes', 'Cancel'],
+    defaultId: 0,
+    cancelId: 1,
+  });
+
+  if (1 == choice) {
+    return;
+  }
+});
+
+ipcRenderer.on('save-html', () => {
+  main.saveHTML(currentWindow, filePath, markdownView.value);
+});
+
+ipcRenderer.on('save-markdown', () => {
+  main.saveMarkdown(currentWindow, filePath, markdownView.value);
+});
+
+markdownView.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+  //alert('a menu will go here some day');
+  // window seems to be a must in macOS
+  markdownContextMenu.popup({ window: currentWindow });
+});
+
+markdownView.addEventListener('dragover', (event) => {
+  const file = getDraggedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    markdownView.classList.add('drag-over');
+  } else {
+    markdownView.classList.add('drag-error');
+  }
+});
+
+markdownView.addEventListener('drop', (event) => {
+  const file = getDroppedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    main.openFile(currentWindow, file.path);
+  } else {
+    alert('That file type is not supported');
+  }
+
+  markdownView.classList.remove('drag-over');
+  markdownView.classList.remove('drag-error');
+});
+
+markdownView.addEventListener('keyup', (event) => {
+  renderMarkdownToHTML(event.target.value);
+  // Whenever the user inputs a keystroke into the Markdown view, checks to see
+  // if the current content matches the content that we stored in a variable
+  // and updates the UI accordingly.
+  updateUserInterface(originalContent !== event.target.value);
+});
+
+newFileButton.addEventListener('click', () => {
+  main.createWindow();
+});
+
+openFileButton.addEventListener('click', () => {
+  main.getFileFromUser(currentWindow);
+});
+
+openInDefaultButton.addEventListener('click', openInDefaultApp);
+
+revertButton.addEventListener('click', () => {
+  markdownView.value = originalContent;
+  renderMarkdownToHTML(originalContent);
+});
+
+saveHtmlButton.addEventListener('click', () => {
+  main.saveHTML(currentWindow, htmlView.innerHTML);
+});
+
+// Adding an event listener to the Save File button
+saveMarkdownButton.addEventListener('click', () => {
+  main.saveMarkdown(currentWindow, filePath, markdownView.value);
+});
+
+showFileButton.addEventListener('click', showFile);
