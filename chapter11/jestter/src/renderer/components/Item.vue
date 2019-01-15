4e63112760695ff29b87243115966e6f4e88e6ef
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-15 12:52:51 +0800

    Listing 11.9 Individual Item component

---
+++ chapter11/jestter/src/renderer/components/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <article class="Item">
+    <label>
+      <input type="checkbox" checked="packed" onChange="onCheckOff">
+      {{ value }}
+    </label>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ["packed", "id", "value", "onCheckOff"]
+};
+</script>
