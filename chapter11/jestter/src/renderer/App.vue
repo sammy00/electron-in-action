83567c9b4f46cc05545db3ee5485d4cb288ce1d7
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-15 12:43:47 +0800

    listing 11.5 a simple component using vue.js

---
+++ chapter11/jestter/src/renderer/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <!--<router-view></router-view>-->
+    <h1>Hello world!</h1>
+    <button class="full-width">This button does not do anything.</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "jestter",
+  metaInfo: {
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" }
+    ]
+  }
+};
+</script>
+
+<style>
+/* CSS */
+</style>
---
+++ chapter11/jestter/src/renderer/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Meta from 'vue-meta'
+
+import App from './App'
+import router from './router'
+import store from './store'
+
+import './style/style.stylus'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.config.productionTip = false
+
+Vue.use(Meta)
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  store,
+  template: '<App/>'
+}).$mount('#app')
