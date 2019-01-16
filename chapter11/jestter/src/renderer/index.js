427c2e30de35e0c506ea0a8547c75e500293e547
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-16 08:49:38 +0800

    rebase the jestter on electron-webpack

--- chapter11/jestter/src/renderer/index.js
+++ chapter11/jestter/src/renderer/index.js
@@ -2,13 +2,9 @@ import Vue from 'vue'
 import Meta from 'vue-meta'
 
 import App from './App'
-import router from './router'
-import store from './store'
 
-// import './style/style.stylus'
 import 'materialize-css/dist/css/materialize.min.css'
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 Vue.use(Meta)
@@ -16,7 +12,5 @@ Vue.use(Meta)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
-  router,
-  store,
-  template: '<App/>'
+  template: '<App/>',
 }).$mount('#app')
