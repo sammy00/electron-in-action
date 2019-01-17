62ffea20a637ccc09befe5463d280f8cae5d1549
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-01-17 12:56:00 +0800

    Listing 13.4 Writing a test to verify the window title

---
+++ chapter13/clipmaster-9000/test/spec.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+const path = require('path')
+const Application = require('spectron').Application
+const electronPath = require('electron')
+
+const app = new Application({
+  path: electronPath,
+  args: [path.join(__dirname, '..')]
+})
+
+describe('Clipmaster 9000', function() {
+  this.timeout(10000)
+
+  beforeEach(() => {
+    return app.start()
+  })
+
+  afterEach(() => {
+    if (app && app.isRunning()) {
+      return app.stop()
+    }
+  })
+
+  //it('should work', () => {
+  //  // Delete this test as soon as you write one of your own.
+  //  assert.ok(true);
+  //});
+
+  it('has the correct title', async () => {
+    const title = await app.client.waitUntilWindowLoaded().getTitle()
+    return assert.equal(title, 'Clipmaster 9000')
+  })
+
+  it('shows an initial window', async () => {
+    const nWindow = await app.client.getWindowCount()
+    return assert.equal(nWindow, 1)
+  })
+})
