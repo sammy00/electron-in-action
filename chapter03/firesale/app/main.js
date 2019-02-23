f61a99d45d7ba2001b344102c6ce4a786ad1597f
sammyne <xiangminli@alumni.sjtu.edu.cn>
2019-02-23 12:16:36 +0800

    Listing 3.11. Opening the Developer Tools from the main process

--- chapter03/firesale/app/main.js
+++ chapter03/firesale/app/main.js
@@ -1,18 +1,25 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require("electron")
 
-let mainWindow = null;
+// Declares mainWindow at the top level so that it won't be collected as garbage
+// after the "ready" event completes
+let mainWindow = null
 
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({ show: false });
+app.on("ready", () => {
+  // show set as false to hide the window
+  mainWindow = new BrowserWindow({ show: false })
 
-  mainWindow.loadFile(__dirname + '/index.html');
+  mainWindow.loadFile(__dirname + "/index.html")
 
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-  });
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+  mainWindow.once("ready-to-show", () => {
+    // show the window once UI is ready
+    mainWindow.show()
+    // open the Developer Tools programmatically
+    mainWindow.webContents.openDevTools()
+  })
+  mainWindow.on("closed", () => {
+    // nullify the window once closed
+    mainWindow = null
+  })
 
   //mainWindow.webContents.openDevTools();
-});
+})
--- chapter03/firesale/app/renderer.js
+++ chapter03/firesale/app/renderer.js
@@ -1,21 +1,22 @@
-const marked = require('marked');
+const marked = require("marked")
 
-const markdownView = document.querySelector('#markdown');
-const htmlView = document.querySelector('#html');
-const newFileButton = document.querySelector('#new-file');
-const openFileButton = document.querySelector('#open-file');
-const saveMarkdownButton = document.querySelector('#save-markdown');
-const revertButton = document.querySelector('#revert');
-const saveHtmlButton = document.querySelector('#save-html');
-const showFileButton = document.querySelector('#show-file');
-const openInDefaultButton = document.querySelector('#open-in-default');
+//  Caching DOM selectors
+const markdownView = document.querySelector("#markdown")
+const htmlView = document.querySelector("#html")
+const newFileButton = document.querySelector("#new-file")
+const openFileButton = document.querySelector("#open-file")
+const saveMarkdownButton = document.querySelector("#save-markdown")
+const revertButton = document.querySelector("#revert")
+const saveHtmlButton = document.querySelector("#save-html")
+const showFileButton = document.querySelector("#show-file")
+const openInDefaultButton = document.querySelector("#open-in-default")
 
-markdownView.addEventListener('keyup', (event) => {
-  console.log('world');
-  renderMarkdownToHTML(event.target.value);
-  console.log('hello');
-});
+// listen on the "keyup" event for re-rendering the HTML when Markdown changes
+markdownView.addEventListener("keyup", (event) => {
+  renderMarkdownToHTML(event.target.value)
+})
 
+// Converting Markdown to HTML
 const renderMarkdownToHTML = (markdown) => {
-  htmlView.innerHTML = marked(markdown, { sanitize: true });
-};
+  htmlView.innerHTML = marked(markdown, { sanitize: true })
+}
